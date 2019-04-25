d7791d8eb184eef8b3caa11b0b108b2df62c0f02
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-25 04:39:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,8 @@ string_with_slashes = ""
 if array.size == 1 
     string_with_slashes = array.to_s
     end_index  = string_with_slashes.length - 2 
-    string_cleaned = string_with_slashes[1,end_index]
-    return string_cleaned
+    return string_with_slashes[1,end_index]
+    
     
   elsif array.size == 2 
   return "#{array[0]} and #{array[1]}"
