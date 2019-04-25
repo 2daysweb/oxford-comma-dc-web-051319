c7b9bfc8df43df49dfbe205eed5f8a5ab4b6b4f0
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-25 04:08:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,12 @@
 def oxford_comma(array)
 string = ""
-if oxford_comma.size > 0 
+if oxford_comma.size == 0 
+    return array.pop.to_s
+  elsif oxford_comma.size == 2 
+  return "#{array[0]} and #{array[1]}
   string = array.join(",")
   return string
-else
-  return array.pop.to_s
+
+
 end
 end
