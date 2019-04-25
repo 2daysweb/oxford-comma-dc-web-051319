1944bbf56fb42942d241d48bd1b9edab2760adaf
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-25 04:29:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ if array.size == 1
     return string_cleaned
     
   elsif array.size == 2 
-  return "#{array[0]} and #{array[1]}
+  return "#{array[0]} and #{array[1]}"
       
   elsif array.size == 3 
   return "#{array[0]}, #{array[1]} and #{array[2]}"
