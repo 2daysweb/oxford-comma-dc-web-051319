aad8e79b335a5bd9423670dba1411acd9981f004
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-04-25 04:28:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,10 @@ if array.size == 1
     return string_cleaned
     
   elsif array.size == 2 
-  return "#{array[0]} and #{array[1]}"
+  return "#{array[0]} and #{array[1]}
+      
+  elsif array.size == 3 
+  return "#{array[0]}, #{array[1]} and #{array[2]}"
 else
   end_index = array.length - 2 
   string = array[1,end_index].join(",")
